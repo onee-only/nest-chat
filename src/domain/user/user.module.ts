a38b80709c00d7cf2a6663637b13386afe4bd6e1
onee-only <kimww0306@gmail.com>
2023-06-10 16:40:18 +0900

    user module 추가

---
+++ src/domain/user/user.module.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './presentation/user.controller';
+import { Module } from '@nestjs/common';
+import { Avatar, User } from './entity';
+import { UserRepository } from './repository/user.repository';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([User, Avatar])],
+    controllers: [UserController],
+    providers: [UserRepository],
+})
+export class UserModule {}
