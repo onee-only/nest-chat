e37b71fd768585eaefa1a873cd6deeb900ce1964
onee-only <kimww0306@gmail.com>
2023-06-12 22:59:30 +0900

    config index 생성

--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -1,2 +1,3 @@
+export { IJwtConfig, JwtConfig, JwtValidationScheme } from './jwt.config';
 export { DatabaseConfig, DatabaseValidationScheme } from './database.config';
 export { LoggerConfig } from './logger.config';
