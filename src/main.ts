9da9ddb6bff63c6c94625f456a712ac45eb49faf
onee-only <kimww0306@gmail.com>
2023-06-15 11:34:45 +0900

    global하게 class-validator 사용

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import { LoggerConfig, SwaggerConfig } from './global/config';
 import { WinstonModule } from 'nest-winston';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: WinstonModule.createLogger(LoggerConfig),
     });
+
+    // validation pipe
+    app.useGlobalPipes(new ValidationPipe());
 
     SwaggerModule.setup(
         'api',
