ace9f4466cb623e157b21e2f54c56d61c8fac96d
onee-only <kimww0306@gmail.com>
2023-06-15 13:10:09 +0900

    UserRepository에 existsByEmail 추가

--- src/domain/user/repository/user.repository.ts
+++ src/domain/user/repository/user.repository.ts
@@ -7,4 +7,8 @@ export class UserRepository extends Repository<User> {
     constructor(private readonly dataSource: DataSource) {
         super(User, dataSource.createEntityManager());
     }
+
+    async existsByEmail(email: string): Promise<boolean> {
+        return await this.exist({ where: { email } });
+    }
 }
