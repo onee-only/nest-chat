56cf6a50e78cbb781963e696ddd67cadc6b4321d
onee-only <kimww0306@gmail.com>
2023-06-15 20:12:44 +0900

    auth module 추가

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailComfirmation } from './entity/email-confirmation.entity';
 import { EmailComfirmationRepository } from './repository/email-confirmation.repository';
+import { AuthSaga } from './saga';
+import { CommandHandlers } from './command/handler';
 
 @Module({
     imports: [
@@ -13,6 +15,15 @@ import { EmailComfirmationRepository } from './repository/email-confirmation.rep
         TypeOrmModule.forFeature([EmailComfirmation]),
     ],
     controllers: [AuthController],
-    providers: [EmailComfirmationRepository],
+    providers: [
+        // saga
+        AuthSaga,
+
+        // handlers
+        ...CommandHandlers,
+
+        // repositories
+        EmailComfirmationRepository,
+    ],
 })
 export class AuthModule {}
