3f05aa432bbf81202feca6a83ac00b0dd7c55afc
onee-only <kimww0306@gmail.com>
2023-06-15 23:17:03 +0900

    email config 추가

---
+++ src/global/config/email.config.ts
@@ -0,0 +1,42 @@
+import { registerAs } from '@nestjs/config';
+import { MailerOptions } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import Joi from 'joi';
+
+export type IEmailConfig = {
+    duration: number;
+    moduleOptions: MailerOptions;
+};
+
+export const EmailValidationScheme = Joi.object({
+    MAIL_EXPIRE_DURATION_MS: Joi.number().required(),
+    MAIL_USER: Joi.string().email().required(),
+    MAIL_PASS: Joi.string().required(),
+});
+
+export const EmailConfig = registerAs(
+    'email',
+    (): IEmailConfig => ({
+        duration: +process.env.EMAIL_EXPIRE_DURATION_MS,
+        moduleOptions: {
+            transport: {
+                host: 'smtp.gmail.com',
+                port: 587,
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                },
+            },
+            defaults: {
+                from: '"simple-chat" <simple@chat.com>',
+            },
+            template: {
+                dir: __dirname + '../../../../resource/mail',
+                adapter: new PugAdapter(),
+                options: {
+                    strict: true,
+                },
+            },
+        },
+    }),
+);
--- src/global/config/index.ts
+++ src/global/config/index.ts
@@ -1,3 +1,8 @@
+export {
+    IEmailConfig,
+    EmailConfig,
+    EmailValidationScheme,
+} from './email.config';
 export { IJwtConfig, JwtConfig, JwtValidationScheme } from './jwt.config';
 export { DatabaseConfig, DatabaseValidationScheme } from './database.config';
 export { LoggerConfig } from './logger.config';
