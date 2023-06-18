e5a84be1e7305447fe955ed26e4c0fa498fa4217
onee-only <kimww0306@gmail.com>
2023-06-19 08:42:43 +0900

    user entity에 before & after insert 제거

--- src/domain/user/entity/user.entity.ts
+++ src/domain/user/entity/user.entity.ts
@@ -1,15 +1,10 @@
 import {
-    AfterInsert,
-    AfterUpdate,
-    BeforeInsert,
-    BeforeUpdate,
     Column,
     CreateDateColumn,
     Entity,
     OneToOne,
     PrimaryGeneratedColumn,
 } from 'typeorm';
-import * as bcrypt from 'bcrypt';
 import { Avatar } from './avatar.entity';
 
 @Entity()
@@ -34,18 +29,4 @@ export class User {
         lazy: true,
     })
     avatar: Avatar;
-
-    @BeforeInsert()
-    @BeforeUpdate()
-    async hashPassword(): Promise<void> {
-        if (this.password != null) {
-            this.password = await bcrypt.hash(this.password, 10);
-        }
-    }
-
-    @AfterInsert()
-    @AfterUpdate()
-    hidePassword(): void {
-        this.password &&= null;
-    }
 }
