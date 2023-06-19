ec671b3d9212b4649671ede2ae2ce9961fd4a281
onee-only <kimww0306@gmail.com>
2023-06-19 15:21:37 +0900

    auth controller에 login route 추가

--- src/domain/auth/presentation/auth.controller.ts
+++ src/domain/auth/presentation/auth.controller.ts
@@ -1,17 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { SignupRequestDto } from './dto/request';
-import { SignupResponseDto } from './dto/response';
-import { CommandBus } from '@nestjs/cqrs';
+import {
+    Body,
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Post,
+    UseInterceptors,
+} from '@nestjs/common';
+import {
+    ApiCreatedResponse,
+    ApiOkResponse,
+    ApiOperation,
+    ApiTags,
+    OmitType,
+} from '@nestjs/swagger';
+import { LoginRequestDto, SignupRequestDto } from './dto/request';
+import { AccessTokenResponseDto, SignupResponseDto } from './dto/response';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { SignupCommand } from '../command';
+import { SetCookieInterceptor } from 'src/global/interceptors/cookie';
+import { LoginQuery } from '../query';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly commandBus: CommandBus) {}
+    constructor(
+        private readonly commandBus: CommandBus,
+        private readonly queryBus: QueryBus,
+    ) {}
 
     @ApiOperation({
-        summary: '회원가입',
+        summary: 'SignUp',
+        description: 'Creates user and sends verification email',
     })
     @ApiCreatedResponse({ type: SignupRequestDto })
     @Post('signup')
@@ -24,6 +43,23 @@ export class AuthController {
                 request.nickname,
                 request.password,
             ),
+        );
+    }
+
+    @ApiOperation({
+        summary: 'LogIn',
+        description:
+            'returns access token and expiration of matching user and sets refresh token to http only cookie',
+    })
+    @ApiOkResponse({ type: OmitType(AccessTokenResponseDto, ['cookies']) })
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @UseInterceptors(SetCookieInterceptor)
+    async login(
+        @Body() request: LoginRequestDto,
+    ): Promise<AccessTokenResponseDto> {
+        return this.queryBus.execute(
+            new LoginQuery(request.email, request.password),
         );
     }
 }
