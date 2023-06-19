0352adef7d444c583789f720e81c6010fd4206ab
onee-only <kimww0306@gmail.com>
2023-06-19 20:21:03 +0900

    send email handler 리팩터링

--- src/domain/auth/command/handler/send-email.handler.ts
+++ src/domain/auth/command/handler/send-email.handler.ts
@@ -2,7 +2,12 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { EmailComfirmationRepository } from '../../repository/email-confirmation.repository';
 import { SendEmailCommand } from '../send-email.command';
 import { ConfigService } from '@nestjs/config';
-import { EmailConfig, IEmailConfig } from 'src/global/config';
+import {
+    EmailConfig,
+    IEmailConfig,
+    IServerConfig,
+    ServerConfig,
+} from 'src/global/config';
 import * as bcrypt from 'bcrypt';
 import { MailerService } from '@nestjs-modules/mailer';
 
@@ -17,20 +22,19 @@ export class SendEmailHandler implements ICommandHandler<SendEmailCommand> {
     async execute(command: SendEmailCommand): Promise<void> {
         const { user } = command;
 
-        const { duration } = this.configService.get<IEmailConfig>(
-            EmailConfig.KEY,
-        );
+        const { duration } = this.getEmailConfig();
+        const { domain, emailVerifyPath } = this.getFrontendConfig();
 
-        const expiration = new Date(Date.now() + duration);
+        const expiresAt = new Date(Date.now() + duration);
         const token = await bcrypt.hash(user.email, 3);
 
-        const emailConfirmation = this.emailConfirmationRepository.create({
-            user,
-            expiration,
-            token,
-        });
-
-        await this.emailConfirmationRepository.save(emailConfirmation);
+        await this.emailConfirmationRepository.save(
+            this.emailConfirmationRepository.create({
+                user: user,
+                expiration: expiresAt,
+                token: token,
+            }),
+        );
 
         await this.mailService.sendMail({
             to: user.email,
@@ -38,9 +42,22 @@ export class SendEmailHandler implements ICommandHandler<SendEmailCommand> {
             template: 'verification.pug',
             context: {
                 nickname: user.avatar.nickname,
-                expiration: expiration.toISOString(),
+                expiration: expiresAt.toISOString(),
                 token: token,
+                domain: domain,
+                path: emailVerifyPath,
             },
         });
+    }
+
+    private getFrontendConfig() {
+        const { frontend } = this.configService.get<IServerConfig>(
+            ServerConfig.KEY,
+        );
+        return frontend;
+    }
+
+    private getEmailConfig() {
+        return this.configService.get<IEmailConfig>(EmailConfig.KEY);
     }
 }
