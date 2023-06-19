a15c54198886334883d84e74c211eb812fefaee5
onee-only <kimww0306@gmail.com>
2023-06-19 15:19:54 +0900

    access token response에 from 메서드 추가

--- src/domain/auth/presentation/dto/response/access-token.response.dto.ts
+++ src/domain/auth/presentation/dto/response/access-token.response.dto.ts
@@ -1,6 +1,27 @@
 import { CookieDto } from 'src/global/interceptors/cookie';
 
-export class AccessTokenResponseDto extends CookieDto {
+type AccessTokenResponseDtoParams = {
     accessToken: string;
     exp: number;
+    cookies: Map<string, string>;
+};
+
+export class AccessTokenResponseDto extends CookieDto {
+    constructor(
+        public readonly accessToken: string,
+        public readonly exp: number,
+        public readonly cookies: Map<string, string>,
+    ) {
+        super(cookies);
+    }
+
+    public static from(
+        params: AccessTokenResponseDtoParams,
+    ): AccessTokenResponseDto {
+        return new AccessTokenResponseDto(
+            params.accessToken,
+            params.exp,
+            params.cookies,
+        );
+    }
 }
