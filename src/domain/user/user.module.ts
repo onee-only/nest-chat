c77adfd2a1ebf8a407ac916b2e6e8a78c6b3ad7e
onee-only <kimww0306@gmail.com>
2023-06-21 15:41:21 +0900

    user module에 query command handler 추가

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -3,13 +3,22 @@ import { UserController } from './presentation/user.controller';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { Avatar, User } from './entity';
-import { UserRepository } from './repository/user.repository';
-import { AvatarRepository } from './repository';
+import { AvatarRepository, UserRepository } from './repository';
+import { CommandHandlers } from './command/handler';
+import { QueryHandlers } from './query/handler';
 
 @Module({
     imports: [CqrsModule, TypeOrmModule.forFeature([User, Avatar])],
     controllers: [UserController],
-    providers: [UserRepository, AvatarRepository],
+    providers: [
+        // handlers
+        ...CommandHandlers,
+        ...QueryHandlers,
+
+        // repositories
+        UserRepository,
+        AvatarRepository,
+    ],
     exports: [UserRepository, AvatarRepository],
 })
 export class UserModule {}
