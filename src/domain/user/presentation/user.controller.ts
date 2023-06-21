c76a959e03b5525504c7e4112aae9415f8233816
onee-only <kimww0306@gmail.com>
2023-06-21 19:51:02 +0900

    get my profile endpoint 추가

--- src/domain/user/presentation/user.controller.ts
+++ src/domain/user/presentation/user.controller.ts
@@ -4,11 +4,12 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/global/decorators';
 import { User } from '../entity';
 import { JwtAuthGuard } from 'src/global/guards';
-import { GetMiniProfileQuery } from '../query';
-import { GetMeResponseDto } from './dto/response';
+import { GetMiniProfileQuery, GetProfileQuery } from '../query';
+import { GetMeResponseDto, GetMyProfileResponseDto } from './dto/response';
 
 @ApiTags('users')
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UserController {
     constructor(
         private readonly commandBus: CommandBus,
@@ -17,12 +18,23 @@ export class UserController {
 
     @ApiOperation({
         summary: 'get me',
-        description: 'Provides information of the current user',
+        description: 'Provides minified information of the current user',
     })
     @ApiOkResponse({ type: GetMeResponseDto })
     @Get('me')
-    @UseGuards(JwtAuthGuard)
     async getMe(@GetUser() user: User): Promise<GetMeResponseDto> {
         return await this.queryBus.execute(new GetMiniProfileQuery(user));
+    }
+
+    @ApiOperation({
+        summary: 'get my profile',
+        description: 'Provides profile information of the current user',
+    })
+    @ApiOkResponse({ type: GetMyProfileResponseDto })
+    @Get('me/profile')
+    async getMyProfile(
+        @GetUser() user: User,
+    ): Promise<GetMyProfileResponseDto> {
+        return this.queryBus.execute(new GetProfileQuery(user));
     }
 }
