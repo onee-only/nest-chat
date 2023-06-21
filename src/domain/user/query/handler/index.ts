7734f0649b18ee1ad39e0055206ac1cd37406faf
onee-only <kimww0306@gmail.com>
2023-06-21 19:48:37 +0900

    get profile handler 추가

--- src/domain/user/query/handler/index.ts
+++ src/domain/user/query/handler/index.ts
@@ -1,1 +1,3 @@
-export const QueryHandlers = [];
+import { GetMiniProfileHandler } from './get-mini-profile.handler';
+import { GetProfileHandler } from './get-profile.handler';
+export const QueryHandlers = [GetMiniProfileHandler, GetProfileHandler];
