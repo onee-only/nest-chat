cd479e1152edde2c9170420737bac4b65d356c86
onee-only <kimww0306@gmail.com>
2023-06-22 16:51:24 +0900

    update profile handler 추가

---
+++ src/domain/user/command/handler/update-profile.handler.ts
@@ -0,0 +1,28 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { UpdateProfileCommand } from '../update-profile.command';
+import { UserRepository } from '../../repository';
+import { GetMyProfileResponseDto } from '../../presentation/dto/response';
+import { ObjectManager } from 'src/global/util/object/object.manager';
+
+@CommandHandler(UpdateProfileCommand)
+export class UpdateProfileHandler
+    implements ICommandHandler<UpdateProfileCommand>
+{
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly objectUtil: ObjectManager,
+    ) {}
+
+    async execute(
+        command: UpdateProfileCommand,
+    ): Promise<GetMyProfileResponseDto> {
+        const { user, data } = command;
+
+        const filtered = this.objectUtil.filterNullish(data);
+        Object.assign(user.avatar, filtered);
+
+        await this.userRepository.save(user);
+
+        return GetMyProfileResponseDto.from(user);
+    }
+}
