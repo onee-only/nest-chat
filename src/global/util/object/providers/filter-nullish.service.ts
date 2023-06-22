ab36890d986c19f84c5b09d473572df364ba0a1c
onee-only <kimww0306@gmail.com>
2023-06-22 13:33:41 +0900

    filter nullish service 리팩터링

--- src/global/util/object/providers/filter-nullish.service.ts
+++ src/global/util/object/providers/filter-nullish.service.ts
@@ -9,25 +9,25 @@ export class NullishFilter {
      * deletes nullish property from the given object. empty object included
      */
     public execute<T extends object>(obj: T): Partial<T> {
-        const filterObj = (obj: object): object => {
-            return Object.fromEntries(
-                Object.entries(obj)
-                    .map(([key, value]) => {
-                        if (value == null) {
+        return this.filterObject(obj);
+    }
+
+    private filterObject(obj: object): object {
+        return Object.fromEntries(
+            Object.entries(obj)
+                .map(([key, value]) => {
+                    if (value == null) {
+                        return null;
+                    }
+                    if (this.validator.isObject(value)) {
+                        if (this.validator.isEmpty(value)) {
                             return null;
                         }
-                        if (this.validator.isObject(value)) {
-                            if (this.validator.isEmpty(value)) {
-                                return null;
-                            }
-                            value = filterObj(value);
-                        }
-                        return [key, value];
-                    })
-                    .filter((value) => value !== null),
-            );
-        };
-
-        return filterObj(obj);
+                        value = this.filterObject(value);
+                    }
+                    return [key, value];
+                })
+                .filter((value) => value !== null),
+        );
     }
 }
