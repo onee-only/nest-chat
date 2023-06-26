4e0d51b760719ee85dadb5d64c5c8d3e9802390c
onee-only <kimww0306@gmail.com>
2023-06-26 13:31:26 +0900

    app module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './domain/auth/auth.module';
 import { UserModule } from './domain/user/user.module';
-import {
-    CaacheModule,
-    ConfigModule,
-    DatabaseModule,
-    MailerModule,
-    StrategyModule,
-} from './global/modules';
+import { ConfigModule, DatabaseModule, MailerModule } from './global/modules';
 
 @Module({
     imports: [
-        // global modules
+        // common modules
         ConfigModule,
         MailerModule,
         DatabaseModule,
-        StrategyModule,
-        CaacheModule,
 
         // app modules
         AuthModule,
