265fcd19434bbeaba26cdd6570a18a6069c69599
onee-only <kimww0306@gmail.com>
2023-06-27 08:12:44 +0900

    index 생성

---
+++ src/domain/room/entity/index.ts
@@ -0,0 +1,4 @@
+export { Room } from './room.entity';
+export { MemberRole } from './member-role.entity';
+export { RoomMember } from './room-member.entity';
+export { Invitation } from './invitation.entity';
