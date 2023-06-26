93f85d7bcc4252f688addcbd4a05fd9db7819021
onee-only <kimww0306@gmail.com>
2023-06-26 19:05:53 +0900

    email confirmation repository에 findWithUserByToken 추가

--- src/domain/auth/repository/email-confirmation.repository.ts
+++ src/domain/auth/repository/email-confirmation.repository.ts
@@ -7,4 +7,13 @@ export class EmailConfirmationRepository extends Repository<EmailConfirmation> {
     constructor(private readonly dataSource: DataSource) {
         super(EmailConfirmation, dataSource.createEntityManager());
     }
+
+    async findWithUserByToken(token: string): Promise<EmailConfirmation> {
+        return await this.findOne({
+            relations: {
+                user: true,
+            },
+            where: { token },
+        });
+    }
 }
