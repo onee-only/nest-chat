0bd317017f4c174e56adbb1d33c5af83e609faa1
onee-only <kimww0306@gmail.com>
2023-06-26 13:32:52 +0900

    refresh jwt guard 로직 추가

--- src/global/guards/jwt/jwt-auth.guard.ts
+++ src/global/guards/jwt/jwt-auth.guard.ts
@@ -3,6 +3,3 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('access') {}
-
-@Injectable()
-export class RefreshAuthGuard extends AuthGuard('refresh') {}
