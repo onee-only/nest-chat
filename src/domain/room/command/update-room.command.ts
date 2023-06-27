6feccf3ab3b29f9576536b354472fc88ed96b721
onee-only <kimww0306@gmail.com>
2023-06-27 20:04:31 +0900

    optional로 바꿈

--- src/domain/room/command/update-room.command.ts
+++ src/domain/room/command/update-room.command.ts
@@ -1,18 +1,18 @@
 import { ICommand } from '@nestjs/cqrs';
 import { User } from 'src/domain/user/entity';
 
-type UpdaetData = {
-    readonly name: string;
-    readonly description: string;
-    readonly profileURL: string;
-    readonly isPublic: boolean;
-    defaultRoleID: number;
+type UpdateData = {
+    readonly name?: string;
+    readonly description?: string;
+    readonly profileURL?: string;
+    readonly isPublic?: boolean;
+    defaultRoleID?: number;
 };
 
 export class UpdateRoomCommand implements ICommand {
     constructor(
         public readonly user: User,
         public readonly roomID: number,
-        public readonly data: UpdaetData,
+        public readonly data: UpdateData,
     ) {}
 }
