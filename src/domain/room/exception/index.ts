8ae133179375a15efc1f0bcf70c01c906ce4be1a
onee-only <kimww0306@gmail.com>
2023-06-27 19:53:26 +0900

    room not found exception 추가

---
+++ src/domain/room/exception/index.ts
@@ -0,0 +1,3 @@
+export { RoomNotFoundException } from './room-not-found.exception';
+export { NoAdminPermissionException } from './no-admin-permission.exception';
+export { NoMatchingRoleException } from './no-matching-role.exception';
