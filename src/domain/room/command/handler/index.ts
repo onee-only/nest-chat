f7855618a232e656dda740ee69918db250cf71d5
onee-only <kimww0306@gmail.com>
2023-06-27 21:00:45 +0900

    delete room handler 추가

--- src/domain/room/command/handler/index.ts
+++ src/domain/room/command/handler/index.ts
@@ -1,3 +1,9 @@
 import { CreateRoomHandler } from './create-room.handler';
+import { DeleteRoomHandler } from './delete-room.handler';
+import { UpdateRoomHandler } from './update-room.handler';
 
-export const CommandHandlers = [CreateRoomHandler];
+export const CommandHandlers = [
+    CreateRoomHandler,
+    UpdateRoomHandler,
+    DeleteRoomHandler,
+];
