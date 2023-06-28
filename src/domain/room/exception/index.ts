54bfc5a9a5b8b7a968ef564f7ff945ef9b87e8fd
onee-only <kimww0306@gmail.com>
2023-06-28 23:24:14 +0900

    index 수정

--- src/domain/room/exception/index.ts
+++ src/domain/room/exception/index.ts
@@ -1,3 +1,5 @@
 export { RoomNotFoundException } from './room-not-found.exception';
 export { NoOwnerPermissionException } from './no-owner-permission.exception';
 export { NoMatchingRoleException } from './no-matching-role.exception';
+export { NoRolePermissionException } from './no-role-permission.exception';
+export { NotRoomMemberException } from './not-room-member.exception';
