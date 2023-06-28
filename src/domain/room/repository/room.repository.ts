333fccc6b5edac99cc1af7821058d742902b09b9
onee-only <kimww0306@gmail.com>
2023-06-28 18:12:43 +0900

    room list에 findList 메서드 추가

--- src/domain/room/repository/room.repository.ts
+++ src/domain/room/repository/room.repository.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { Room } from '../entity';
+import { Room, RoomMember } from '../entity';
+import { User } from 'src/domain/user/entity';
+import { ListOptions } from '../query';
+
+type ListResult = {
+    list: any[];
+    count: number;
+};
 
 @Injectable()
 export class RoomRepository extends Repository<Room> {
@@ -13,5 +20,73 @@ export class RoomRepository extends Repository<Room> {
             where: { id },
             relations: { owner: true },
         });
+    }
+
+    async findList(user: User, options: ListOptions): Promise<ListResult> {
+        const {
+            order,
+            orderDir,
+            endDate,
+            startDate,
+            query,
+            page = 1,
+            size = 20,
+        } = options;
+
+        const qb = this.createQueryBuilder('room')
+            .where('room.isPublic = true')
+            .andWhere(this.optional(query, 'room.name like :query'), {
+                query: `%${query}%`,
+            })
+            .andWhere(this.optional(endDate, 'room.createdAt <= :endDate'), {
+                endDate,
+            })
+            .andWhere(this.optional(startDate, 'room.createdAt >= :endDate'), {
+                startDate,
+            });
+
+        const count = await qb.getCount();
+
+        const list = await qb
+            .select('room')
+            .addSelect('count(*)', 'count')
+            .addSelect(
+                (subquery) =>
+                    subquery
+                        .select(['user.isMember', 'members.count'])
+                        .from(
+                            (subquery) =>
+                                subquery
+                                    .select('count(*) > 0', 'isMember')
+                                    .from(RoomMember, 'roomMember')
+                                    .where('roomMember.userId = :userID')
+                                    .andWhere('roomMember.roomId = room.id'),
+                            'user',
+                        )
+                        .addFrom(
+                            (subquery) =>
+                                subquery
+                                    .select('count(*) > 0', 'count')
+                                    .from(RoomMember, 'roomMember')
+                                    .where('roomMember.userId = :userID'),
+                            'members',
+                        )
+                        .setParameter('userID', user.id),
+                'member',
+            )
+            .innerJoinAndSelect('room.owner', 'owner')
+            .orderBy(order, orderDir)
+            .offset(size * (page - 1))
+            .limit(size)
+            .getRawMany();
+
+        return {
+            list,
+            count,
+        };
+    }
+
+    private optional(thing: any, query: string): string {
+        return thing ? query : '1=1';
     }
 }
