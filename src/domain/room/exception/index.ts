254298c9f924dbf79c0ee366fefe743e87699f13
onee-only <kimww0306@gmail.com>
2023-06-29 12:13:32 +0900

    duplicate role alias exception 추가

--- src/domain/room/exception/index.ts
+++ src/domain/room/exception/index.ts
@@ -3,3 +3,4 @@ export { NoOwnerPermissionException } from './no-owner-permission.exception';
 export { NoMatchingRoleException } from './no-matching-role.exception';
 export { NoRolePermissionException } from './no-role-permission.exception';
 export { NotRoomMemberException } from './not-room-member.exception';
+export { DuplicateRoleAliasException } from './duplicate-role-alias.exception';
