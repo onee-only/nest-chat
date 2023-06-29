57b898c679fb9a74e57b796de1d20157a381f7d2
onee-only <kimww0306@gmail.com>
2023-06-29 12:13:05 +0900

    member role repository에 duplicateAliasExists 메서드 추가

--- src/domain/room/repository/member-role.repository.ts
+++ src/domain/room/repository/member-role.repository.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { MemberRole } from '../entity';
+import { MemberRole, Room } from '../entity';
 
 @Injectable()
 export class MemberRoleRepository extends Repository<MemberRole> {
     constructor(private readonly dataSource: DataSource) {
         super(MemberRole, dataSource.createEntityManager());
+    }
+
+    async duplicateAliasExists(room: Room, alias: string): Promise<boolean> {
+        return await this.exist({ where: { room, alias } });
     }
 }
