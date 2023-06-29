2712c2fb2ece07558dedd13afc4173f162fb37ac
onee-only <kimww0306@gmail.com>
2023-06-29 12:12:21 +0900

    update role request dto 추가

--- src/domain/room/presentation/dto/request/index.ts
+++ src/domain/room/presentation/dto/request/index.ts
@@ -1,3 +1,4 @@
 export { CreateRoomRequestDto } from './create-room.request.dto';
 export { CreateRoleRequestDto } from './create-role.request.dto';
 export { UpdateRoomRequestDto } from './update-room.request.dto';
+export { UpdateRoleRequestDto } from './update-role.request.dto';
