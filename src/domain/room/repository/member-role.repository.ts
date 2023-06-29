e5c48d6225cbf8e891bb75abc8dae3e57c5a1e6d
onee-only <kimww0306@gmail.com>
2023-06-29 19:18:52 +0900

    room repository 리팩터링

--- src/domain/room/repository/member-role.repository.ts
+++ src/domain/room/repository/member-role.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { MemberRole, Room } from '../entity';
+import { MemberRole, Room, RoomMember } from '../entity';
+import { RoleElement } from '../presentation/dto/response';
 
 @Injectable()
 export class MemberRoleRepository extends Repository<MemberRole> {
@@ -10,5 +11,18 @@ export class MemberRoleRepository extends Repository<MemberRole> {
 
     async duplicateAliasExists(room: Room, alias: string): Promise<boolean> {
         return await this.exist({ where: { room, alias } });
+    }
+
+    async findWithMemberCountByRoom(room: Room): Promise<RoleElement[]> {
+        return await this.createQueryBuilder('role')
+            .select('role.*')
+            .addSelect((subquery) =>
+                subquery
+                    .select('count(*)', 'memberCount')
+                    .from(RoomMember, 'roomMember')
+                    .where('roomMember.roleId = role.id'),
+            )
+            .where('role.roomId = :roomID', { roomID: room.id })
+            .getRawMany<RoleElement>();
     }
 }
