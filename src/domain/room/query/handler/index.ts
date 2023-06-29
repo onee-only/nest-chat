dff18255c12b65c145712b5792e9a144cd75dbae
onee-only <kimww0306@gmail.com>
2023-06-29 20:40:56 +0900

    list memeber handler 추가

--- src/domain/room/query/handler/index.ts
+++ src/domain/room/query/handler/index.ts
@@ -1,4 +1,9 @@
+import { ListMemberHandler } from './list-member.handler';
 import { ListRoleHandler } from './list-role.handler';
 import { ListRoomHandler } from './list-room.handler';
 
-export const QueryHandlers = [ListRoleHandler, ListRoomHandler];
+export const QueryHandlers = [
+    ListRoleHandler,
+    ListRoomHandler,
+    ListMemberHandler,
+];
