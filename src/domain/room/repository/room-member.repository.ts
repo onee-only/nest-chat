6c9c23a0addb9d6dd9d1e5e5e41eb3c48fb9db48
onee-only <kimww0306@gmail.com>
2023-06-29 16:46:02 +0900

    room member repository에 existsByRoomAndRole 추가

--- src/domain/room/repository/room-member.repository.ts
+++ src/domain/room/repository/room-member.repository.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { RoomMember } from '../entity';
+import { MemberRole, Room, RoomMember } from '../entity';
 
 @Injectable()
 export class RoomMemberRepository extends Repository<RoomMember> {
     constructor(private readonly dataSource: DataSource) {
         super(RoomMember, dataSource.createEntityManager());
+    }
+
+    async existsByRoomAndRole(room: Room, role: MemberRole): Promise<boolean> {
+        return await this.exist({ where: { role, room } });
     }
 }
