9adec28af28c1cdfdb453c275b88bc5ef615b439
onee-only <kimww0306@gmail.com>
2023-06-30 11:53:28 +0900

    create invitation request dto 추가

--- src/domain/room/presentation/dto/request/index.ts
+++ src/domain/room/presentation/dto/request/index.ts
@@ -2,3 +2,4 @@ export { CreateRoomRequestDto } from './create-room.request.dto';
 export { CreateRoleRequestDto } from './create-role.request.dto';
 export { UpdateRoomRequestDto } from './update-room.request.dto';
 export { UpdateRoleRequestDto } from './update-role.request.dto';
+export { CreateInvitationRequestDto } from './create-invitation.request.dto';
