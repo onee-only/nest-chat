c35e1ef02298e9d1ba8e314514e4a476266e3ff6
onee-only <kimww0306@gmail.com>
2023-07-01 19:46:32 +0900

    delete invitation handler 추가

---
+++ src/domain/room/command/handler/delete-invitation.handler.ts
@@ -0,0 +1,43 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DeleteInvitationCommand } from '../delete-invitation.command';
+import { InvitationRepository, RoomRepository } from '../../repository';
+import {
+    NoMatchingInvitationException,
+    NoOwnerPermissionException,
+    RoomNotFoundException,
+} from '../../exception';
+
+@CommandHandler(DeleteInvitationCommand)
+export class DeleteInvitationHandler
+    implements ICommandHandler<DeleteInvitationCommand>
+{
+    constructor(
+        private readonly roomRepository: RoomRepository,
+        private readonly invitationRepository: InvitationRepository,
+    ) {}
+
+    async execute(command: DeleteInvitationCommand): Promise<void> {
+        const { token, roomID, user } = command;
+
+        const room = await this.roomRepository
+            .findOneWithOwnerById(roomID)
+            .catch(() => {
+                throw new RoomNotFoundException(roomID);
+            });
+
+        const invitation = await this.invitationRepository
+            .findOneByOrFail({
+                token,
+                room,
+            })
+            .catch(() => {
+                throw new NoMatchingInvitationException(roomID, token);
+            });
+
+        if (room.owner != user) {
+            throw new NoOwnerPermissionException();
+        }
+
+        await this.invitationRepository.delete(invitation);
+    }
+}
