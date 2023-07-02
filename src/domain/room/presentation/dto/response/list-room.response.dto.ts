cc014de06b4272b7f907811a5fad699fa9dcc0a5
onee-only <kimww0306@gmail.com>
2023-07-02 17:04:43 +0900

    list room response 수정

--- src/domain/room/presentation/dto/response/list-room.response.dto.ts
+++ src/domain/room/presentation/dto/response/list-room.response.dto.ts
@@ -10,23 +10,13 @@ type ListInfo = {
 export class ListRoomResponseDto {
     constructor(
         public readonly rooms: RoomListElementDto[],
-        public readonly pageNum: number,
-        public readonly pageSize: number,
-        public readonly totalPages: number,
-        public readonly totalRooms: number,
+        public readonly pageInfo: ListInfo,
     ) {}
 
     public static from(
         rooms: RoomListElementDto[],
         pageInfo: ListInfo,
     ): ListRoomResponseDto {
-        const { pageNum, totalPages, totalRooms, pageSize } = pageInfo;
-        return new ListRoomResponseDto(
-            rooms,
-            pageNum,
-            pageSize,
-            totalPages,
-            totalRooms,
-        );
+        return new ListRoomResponseDto(rooms, pageInfo);
     }
 }
