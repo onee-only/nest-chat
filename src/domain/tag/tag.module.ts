28c2fb1b8dbfc037a8b0fe097f323e497cc8fc60
onee-only <kimww0306@gmail.com>
2023-07-03 16:51:42 +0900

    chore

--- src/domain/tag/tag.module.ts
+++ src/domain/tag/tag.module.ts
@@ -3,10 +3,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './entity/tag.entity';
 import { TagRepository } from './repository/tag.repository';
+import { ListTagHandler } from './query/handler';
 
 @Module({
     imports: [CqrsModule, TypeOrmModule.forFeature([Tag])],
-    providers: [TagRepository],
+    providers: [ListTagHandler, TagRepository],
     exports: [TagRepository],
 })
 export class TagModule {}
