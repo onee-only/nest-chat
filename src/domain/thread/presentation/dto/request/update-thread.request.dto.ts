5e1fee720d2d55c888916743b0b292dc3877664f
onee-only <kimww0306@gmail.com>
2023-07-03 14:49:46 +0900

    update thread request에 ApiProperty decorator 추가

--- src/domain/thread/presentation/dto/request/update-thread.request.dto.ts
+++ src/domain/thread/presentation/dto/request/update-thread.request.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateThreadRequestDto {
+    @ApiProperty()
     @IsOptional()
     @IsString()
     title?: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsString({ each: true })
     tags?: string[];
