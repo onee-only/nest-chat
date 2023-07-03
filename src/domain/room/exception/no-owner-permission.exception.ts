8e0ffdd3f8802e69bd69f303c8c1cfbcb3ae2fb2
onee-only <kimww0306@gmail.com>
2023-07-03 11:54:53 +0900

    chore

--- src/domain/room/exception/no-owner-permission.exception.ts
+++ src/domain/room/exception/no-owner-permission.exception.ts
@@ -2,6 +2,6 @@ import { ForbiddenException } from '@nestjs/common';
 
 export class NoOwnerPermissionException extends ForbiddenException {
     constructor() {
-        super('you should be an owner for this action');
+        super('you should be an owner of room for this action');
     }
 }
