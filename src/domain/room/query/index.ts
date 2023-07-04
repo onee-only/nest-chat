e63f826624af4046c087f01ef57dbbb1fd15aba7
onee-only <kimww0306@gmail.com>
2023-07-04 10:03:00 +0900

    list pinned thread qeury 추가

--- src/domain/room/query/index.ts
+++ src/domain/room/query/index.ts
@@ -1,5 +1,5 @@
 export { ListRoomQuery, ListOptions } from './list-room.query';
+export { RetreiveRoomQuery } from './retreive-room.query';
 export { ListRoleQuery } from './list-role.query';
 export { ListMemberQuery } from './list-member.query';
 export { ListInvitationQuery } from './list-invitation.query';
-export { RetreiveRoomQuery } from './retreive-room.query';
--- src/domain/thread/query/index.ts
+++ src/domain/thread/query/index.ts
@@ -1,1 +1,2 @@
 export { ListThreadQuery, ListOptions } from './list-thread.query';
+export { ListPinnedThreadQuery } from './list-pinned-thread.query';
