fac3e192ed3245fd72025cad6c5a01bdf6e70470
onee-only <kimww0306@gmail.com>
2023-07-06 19:41:12 +0900

    websocket exception 추가

--- src/domain/thread/exception/index.ts
+++ src/domain/thread/exception/index.ts
@@ -1,2 +1,5 @@
 export { NoMathcingThreadException } from './no-matching-thread.exception';
 export { NoCreatorPermissionException } from './no-creator-permission.exception';
+export { WsRoomNotFoundException } from './ws-room-not-found.exception';
+export { WsNoMatchingThreadException } from './ws-no-matching-thread.exception';
+export { WsNotRoomMemberException } from './ws-not-room-member.exception';
