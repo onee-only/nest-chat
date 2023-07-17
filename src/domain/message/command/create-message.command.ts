fddd8c2ae0d138172a345e0aa42756f7571e7585
onee-only <kimww0306@gmail.com>
2023-07-17 14:07:54 +0900

    create message command param 분리

--- src/domain/message/command/create-message.command.ts
+++ src/domain/message/command/create-message.command.ts
@@ -1,12 +1,17 @@
 import { ICommand } from '@nestjs/cqrs';
 import { User } from 'src/domain/user/entity';
 
+type MessageInput = {
+    readonly body: string;
+    readonly replyTo?: number;
+    readonly embedments?: Array<Express.Multer.File>;
+};
+
 export class CreateMessageCommand implements ICommand {
     constructor(
         public readonly roomID: number,
         public readonly threadID: number,
         public readonly user: User,
-        public readonly body: string,
-        public readonly replyTo?: number,
+        public readonly data: MessageInput,
     ) {}
 }
