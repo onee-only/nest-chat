eb2dd484be0208a3c7c4be970aaf1f0fbbf87b93
onee-only <kimww0306@gmail.com>
2023-07-17 14:15:40 +0900

    create message 로직 수정

--- src/domain/message/command/handler/create-message.handler.ts
+++ src/domain/message/command/handler/create-message.handler.ts
@@ -1,6 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreateMessageCommand } from '../create-message.command';
-import { MessageRepository } from '../../repository';
+import { EmbedmentRepository, MessageRepository } from '../../repository';
 import { PermissionChecker } from 'src/domain/room/util';
 import {
     MemberRoleRepository,
@@ -13,6 +13,10 @@ import { NoMathcingThreadException } from 'src/domain/thread/exception';
 import { RoomPermission } from 'src/domain/room/enum';
 import { MessageParser } from '../../util';
 import { NoMatchingMessageException } from '../../exception';
+import { MemberRole, Room, RoomMember } from 'src/domain/room/entity';
+import { Embedment, Message } from '../../entity';
+import { Thread } from 'src/domain/thread/entity';
+import { StorageManager, UploadResult } from 'src/global/modules/utils';
 
 @CommandHandler(CreateMessageCommand)
 export class CreateMessageHandler
@@ -25,13 +29,20 @@ export class CreateMessageHandler
 
         private readonly threadRepository: ThreadRepository,
         private readonly messageRepository: MessageRepository,
+        private readonly embedementsRepository: EmbedmentRepository,
 
         private readonly permissionChecker: PermissionChecker,
         private readonly messageParser: MessageParser,
+        private readonly storageManager: StorageManager,
     ) {}
 
     async execute(command: CreateMessageCommand): Promise<void> {
-        const { roomID, threadID, user, body, replyTo } = command;
+        const {
+            roomID,
+            threadID,
+            user,
+            data: { body, replyTo, embedments },
+        } = command;
 
         const room = await this.roomRepository
             .findOneByOrFail({ id: roomID })
@@ -58,31 +69,76 @@ export class CreateMessageHandler
             content: body,
         });
 
-        const mentionNames = this.messageParser.parse(body);
-        if (mentionNames.length !== 0) {
-            message.mentionMembers =
-                await this.roomMemberRepository.findByRoomAndUserNicknames(
-                    room,
-                    mentionNames,
-                );
+        const names = this.messageParser.parse(body);
+        message.mentionMembers = await this.findMentionMembers(names, room);
+        message.mentionRoles = await this.findMentionRoles(names, room);
 
-            message.mentionRoles =
-                await this.memberRoleRepository.findByRoomAndAliases(
-                    room,
-                    mentionNames,
-                );
-        }
+        const results = await this.storageManager.uploadFiles(
+            'messages/embedments',
+            embedments,
+        );
+        message.embedments = this.createEmbedments(message, results);
 
-        if (replyTo !== undefined) {
-            message.replyTo = await this.messageRepository
-                .findOneByOrFail({ thread, id: replyTo })
-                .catch(() => {
-                    throw new NoMatchingMessageException(threadID, replyTo);
-                });
-        }
+        message.replyTo = message.replyTo = await this.findReplyTo(
+            replyTo,
+            thread,
+        );
 
         await this.messageRepository.save(message);
 
         // should publish event
+    }
+
+    private async findMentionMembers(
+        mentionNames: string[],
+        room: Room,
+    ): Promise<RoomMember[]> {
+        if (mentionNames.length !== 0) {
+            return await this.roomMemberRepository.findByRoomAndUserNicknames(
+                room,
+                mentionNames,
+            );
+        }
+        return null;
+    }
+
+    private async findMentionRoles(
+        mentionNames: string[],
+        room: Room,
+    ): Promise<MemberRole[]> {
+        if (mentionNames.length !== 0) {
+            return await this.memberRoleRepository.findByRoomAndAliases(
+                room,
+                mentionNames,
+            );
+        }
+        return null;
+    }
+
+    private async findReplyTo(
+        replyTo: number,
+        thread: Thread,
+    ): Promise<Message> {
+        if (replyTo !== undefined) {
+            return await this.messageRepository
+                .findOneByOrFail({ thread, id: replyTo })
+                .catch(() => {
+                    throw new NoMatchingMessageException(thread.id, replyTo);
+                });
+        }
+        return null;
+    }
+
+    private createEmbedments(
+        message: Message,
+        results: UploadResult[],
+    ): Embedment[] {
+        return results.map(({ key, originalName }) =>
+            this.embedementsRepository.create({
+                key,
+                message,
+                name: originalName,
+            }),
+        );
     }
 }
