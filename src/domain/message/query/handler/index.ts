9202222acd5206fec6b83f49150a9fde975f78f9
onee-only <kimww0306@gmail.com>
2023-07-19 15:38:15 +0900

    subscribe message handler 추가

---
+++ src/domain/message/query/handler/index.ts
@@ -0,0 +1,3 @@
+import { SubscribeMessageHandler } from './subscribe-message.handler';
+
+export const QueryHandlers = [SubscribeMessageHandler];
---
+++ src/domain/message/query/handler/subscribe-message.handler.ts
@@ -0,0 +1,48 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { SubscribeMessageQuery } from '../subscribe-message.query';
+import { Observable } from 'rxjs';
+import { MessagePayload } from 'src/domain/thread/util/chat/types';
+import { ChatBroker } from 'src/domain/thread/util/chat';
+import { RoomRepository } from 'src/domain/room/repository';
+import { ThreadRepository } from 'src/domain/thread/repository';
+import { RoomNotFoundException } from 'src/domain/room/exception';
+import { NoMathcingThreadException } from 'src/domain/thread/exception';
+import { PermissionChecker } from 'src/domain/room/util';
+
+@QueryHandler(SubscribeMessageQuery)
+export class SubscribeMessageHandler
+    implements IQueryHandler<SubscribeMessageQuery>
+{
+    constructor(
+        private readonly roomRepository: RoomRepository,
+        private readonly threadRepository: ThreadRepository,
+
+        private readonly chatBroker: ChatBroker,
+        private readonly permissionChecker: PermissionChecker,
+    ) {}
+
+    async execute(
+        query: SubscribeMessageQuery,
+    ): Promise<Observable<MessagePayload>> {
+        const { roomID, threadID, user } = query;
+
+        const room = await this.roomRepository
+            .findOneByOrFail({ id: roomID })
+            .catch(() => {
+                throw new RoomNotFoundException(roomID);
+            });
+
+        const thread = await this.threadRepository
+            .findOneByOrFail({ id: threadID })
+            .catch(() => {
+                throw new NoMathcingThreadException(roomID, threadID);
+            });
+
+        await this.permissionChecker.checkOrThrow({
+            room,
+            user,
+        });
+
+        return await this.chatBroker.subscribe(room.id, thread.id);
+    }
+}
