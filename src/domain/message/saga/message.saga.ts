8667090179cf7b37017014139cddcb0963015026
onee-only <kimww0306@gmail.com>
2023-07-24 08:36:54 +0900

    notify member mention saga 추가

--- src/domain/message/saga/message.saga.ts
+++ src/domain/message/saga/message.saga.ts
@@ -8,7 +8,10 @@ import {
     RoleMentionedEvent,
 } from '../event';
 import { PublishMessageCommand } from '../command';
-import { NotifyRoleMentionCommand } from 'src/domain/notification/command';
+import {
+    NotifyMemberMentionCommand,
+    NotifyRoleMentionCommand,
+} from 'src/domain/notification/command';
 
 @Injectable()
 export class MessageSaga {
@@ -35,17 +38,17 @@ export class MessageSaga {
         );
     }
 
-    // @Saga()
-    // notifyMemberMention(events$: Observable<any>): Observable<ICommand> {
-    //     return events$.pipe(
-    //         ofType(MemberMentionedEvent),
-    //         filter(({ members }: MemberMentionedEvent) => members.length !== 0),
-    //         map(
-    //             ({ room, members, message }: MemberMentionedEvent) =>
-    //                 new SendEmailCommand(user),
-    //         ),
-    //     );
-    // }
+    @Saga()
+    notifyMemberMention(events$: Observable<any>): Observable<ICommand> {
+        return events$.pipe(
+            ofType(MemberMentionedEvent),
+            filter(({ members }: MemberMentionedEvent) => members.length !== 0),
+            map(
+                ({ room, members, message }: MemberMentionedEvent) =>
+                    new NotifyMemberMentionCommand(room, members, message),
+            ),
+        );
+    }
 
     // @Saga()
     // notifyReply(events$: Observable<any>): Observable<ICommand> {
