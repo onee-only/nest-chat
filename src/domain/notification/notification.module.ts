4b0a9df567bc1f976b1bc491284a83d0b96db5ce
김원욱 <oneee@gim-won-ug-ui-MacBookAir.local>
2023-07-24 08:06:33 +0900

    chore

--- src/domain/notification/notification.module.ts
+++ src/domain/notification/notification.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entity';
+import { NotificationRepository } from './repository';
+import { RoomModule } from '../room/room.module';
+import { CommandHandlers } from './command/handler';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Notification])],
-    controllers: [],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([Notification]), RoomModule],
+    providers: [
+        // handlers
+        ...CommandHandlers,
+
+        // repositories
+        NotificationRepository,
+    ],
 })
 export class NotificationModule {}
