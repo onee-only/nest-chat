daac3716faea26a7431880fbf528240b3d018c8d
onee-only <kimww0306@gmail.com>
2023-07-24 22:59:53 +0900

    notification module에 query handlers 추가

--- src/domain/notification/notification.module.ts
+++ src/domain/notification/notification.module.ts
@@ -5,12 +5,14 @@ import { NotificationRepository } from './repository';
 import { RoomModule } from '../room/room.module';
 import { CommandHandlers } from './command/handler';
 import { NotifiactionPublisher } from './util/notification.publisher';
+import { QueryHandlers } from './query/handler';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Notification]), RoomModule],
     providers: [
         // handlers
         ...CommandHandlers,
+        ...QueryHandlers,
 
         // utils
         NotifiactionPublisher,
