4bbac0cdca8b84c20c69b01badb74b8cdd9f9501
onee-only <kimww0306@gmail.com>
2023-07-25 08:23:33 +0900

    clear notifications command 추가

--- src/domain/notification/command/index.ts
+++ src/domain/notification/command/index.ts
@@ -1,3 +1,4 @@
 export { NotifyRoleMentionCommand } from './notify-role-mention.command';
 export { NotifyMemberMentionCommand } from './notify-member-mention.command';
 export { NotifyReplyCommand } from './notify-reply.command';
+export { ClearNotificationsCommand } from './clear-notifications.command';
