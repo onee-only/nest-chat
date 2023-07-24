9db225e7624ff73415813aa682dfb4b84e817978
onee-only <kimww0306@gmail.com>
2023-07-24 20:23:57 +0900

    notification module에 notification publisher 추가

--- src/domain/notification/notification.module.ts
+++ src/domain/notification/notification.module.ts
@@ -4,12 +4,16 @@ import { Notification } from './entity';
 import { NotificationRepository } from './repository';
 import { RoomModule } from '../room/room.module';
 import { CommandHandlers } from './command/handler';
+import { NotifiactionPublisher } from './util/notification.publisher';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Notification]), RoomModule],
     providers: [
         // handlers
         ...CommandHandlers,
+
+        // utils
+        NotifiactionPublisher,
 
         // repositories
         NotificationRepository,
