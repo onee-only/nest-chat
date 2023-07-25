2f9a1b2497e1d652246ff758d82b51ec5d77651b
onee-only <kimww0306@gmail.com>
2023-07-25 09:35:34 +0900

    delete notification command 추가

--- src/domain/notification/command/index.ts
+++ src/domain/notification/command/index.ts
@@ -2,3 +2,4 @@ export { NotifyRoleMentionCommand } from './notify-role-mention.command';
 export { NotifyMemberMentionCommand } from './notify-member-mention.command';
 export { NotifyReplyCommand } from './notify-reply.command';
 export { ClearNotificationsCommand } from './clear-notifications.command';
+export { DeleteNotificationCommand } from './delete-notification.command';
