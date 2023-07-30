db488fd43ac504aa3df4656f3272942d4448e5b1
onee-only <kimww0306@gmail.com>
2023-07-30 20:19:13 +0900

    list message handler 추가

--- src/domain/message/query/handler/index.ts
+++ src/domain/message/query/handler/index.ts
@@ -1,3 +1,4 @@
+import { ListMessageHandler } from './list-message.handler';
 import { SubscribeMessageHandler } from './subscribe-message.handler';
 
-export const QueryHandlers = [SubscribeMessageHandler];
+export const QueryHandlers = [SubscribeMessageHandler, ListMessageHandler];
---
+++ src/domain/message/query/handler/list-message.handler.ts
@@ -0,0 +1,62 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { ListMessageQuery } from '../list-message.query';
+import { ListMessageResponseDto } from '../../presentation/dto/response';
+import { RoomRepository } from 'src/domain/room/repository';
+import { ThreadRepository } from 'src/domain/thread/repository';
+import { MessageRepository } from '../../repository';
+import { RoomNotFoundException } from 'src/domain/room/exception';
+import { NoMathcingThreadException } from 'src/domain/thread/exception';
+import { LessThan } from 'typeorm';
+import { PermissionChecker } from 'src/domain/room/util';
+
+@QueryHandler(ListMessageQuery)
+export class ListMessageHandler implements IQueryHandler<ListMessageQuery> {
+    constructor(
+        private readonly permissionChecker: PermissionChecker,
+
+        private readonly roomRepository: RoomRepository,
+        private readonly threadRepository: ThreadRepository,
+        private readonly messageRepository: MessageRepository,
+    ) {}
+
+    async execute(query: ListMessageQuery): Promise<ListMessageResponseDto> {
+        const {
+            user,
+            options: { endDate, limit, roomID, threadID },
+        } = query;
+
+        const room = await this.roomRepository
+            .findOneByOrFail({
+                id: roomID,
+            })
+            .catch(() => {
+                throw new RoomNotFoundException(roomID);
+            });
+
+        const thread = await this.threadRepository
+            .findOneByOrFail({
+                id: threadID,
+                room,
+            })
+            .catch(() => {
+                throw new NoMathcingThreadException(roomID, threadID);
+            });
+
+        await this.permissionChecker.checkOrThrow({
+            room: room,
+            user: user,
+        });
+
+        const messages = await this.messageRepository.find({
+            where: { thread: thread, createdAt: LessThan(endDate) },
+            relations: {
+                author: { avatar: true },
+                embedments: true,
+                replyTo: true,
+            },
+            take: limit,
+        });
+
+        return ListMessageResponseDto.from(messages);
+    }
+}
