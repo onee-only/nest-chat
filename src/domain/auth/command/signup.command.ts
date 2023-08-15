03a75a60c9a9e76dc6db5d45958d2635cb602c22
onee-only <kimww0306@gmail.com>
2023-08-15 19:02:12 +0900

    signup command nickname password 위치 변경

--- src/domain/auth/command/signup.command.ts
+++ src/domain/auth/command/signup.command.ts
@@ -3,7 +3,7 @@ import { ICommand } from '@nestjs/cqrs';
 export class SignupCommand implements ICommand {
     constructor(
         readonly email: string,
-        readonly password: string,
         readonly nickname: string,
+        readonly password: string,
     ) {}
 }
