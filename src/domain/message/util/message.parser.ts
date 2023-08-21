10ccd850e4c94141a03ea897fbc52ddbbb031a72
onee-only <kimww0306@gmail.com>
2023-08-21 19:48:20 +0900

    message parser fixed

--- src/domain/message/util/message.parser.ts
+++ src/domain/message/util/message.parser.ts
@@ -5,6 +5,9 @@ export class MessageParser {
     private readonly regex = /\B@\w+/g;
 
     parse(body: string): string[] {
-        return body.match(this.regex).map((result) => result.replace('@', ''));
+        return (
+            body.match(this.regex)?.map((result) => result.replace('@', '')) ??
+            []
+        );
     }
 }
