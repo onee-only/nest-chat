30128c2fdc3b348446ec217d723e1c7ecfd4eb90
onee-only <kimww0306@gmail.com>
2023-08-25 20:15:04 +0900

    update password command 추가

--- src/domain/auth/command/index.ts
+++ src/domain/auth/command/index.ts
@@ -2,3 +2,4 @@ export * from './signup.command';
 export * from './send-email.command';
 export * from './logout.command';
 export * from './verify-email.command';
+export * from './update-password.command';
