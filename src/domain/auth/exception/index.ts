d892c315cb1bc5a5826d793d99f3113789ec756b
onee-only <kimww0306@gmail.com>
2023-08-25 20:14:47 +0900

    invalid password exception cnrk

--- src/domain/auth/exception/index.ts
+++ src/domain/auth/exception/index.ts
@@ -2,3 +2,4 @@ export * from './duplicate-email.exception';
 export * from './invalid-credentials.exception';
 export * from './invalid-email-token.exception';
 export * from './unverified-email.exception';
+export * from './invalid-password.exception';
